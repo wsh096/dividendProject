334621bb029cc962c6e4fdf14882e26124f33c7d
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-03-18 18:25:38 +0900

    cokacola 부분만 콘솔에 출력되는 형태의 학습

--- src/main/java/com/example/mission3/Mission3Application.java
+++ src/main/java/com/example/mission3/Mission3Application.java
@@ -1,13 +1,46 @@
 package com.example.mission3;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+import java.io.IOException;
+
+//@SpringBootApplication
 public class Mission3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Mission3Application.class, args);
+       // SpringApplication.run(Mission3Application.class, args);
+
+        try {
+            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1679097600&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+            Document document = connection.get();
+
+            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element ele = eles.get(0); //table 전체
+
+            Element tbody =ele.children().get(1);
+            for(Element e : tbody.children()){
+                String txt = e.text();
+                if(!txt.endsWith("Dividend")){
+                    continue;
+                }
+                String [] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",",""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+                System.out.println(year+"/"+month+"/"+day+" -> " +dividend);
+            }
+            //System.out.println(ele);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
