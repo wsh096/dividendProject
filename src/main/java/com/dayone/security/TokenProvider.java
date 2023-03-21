46f658ab8852545e1c1547b51a88e926a416a3d7
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-03-22 00:43:27 +0900

    Jwt Token 만들기. 암호화 복호화 하는 과정의 인증키 생성. HS512로 생성

--- src/main/java/com/dayone/security/TokenProvider.java
+++ src/main/java/com/dayone/security/TokenProvider.java
@@ -1,4 +1,62 @@
 package com.dayone.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+
+import org.jsoup.helper.StringUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final String KEY_ROLES = "roles";
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; //1초 * 60 * 60 = 1시간
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    /**
+     * 토큰생성(발급)
+     *
+     * @param username
+     * @param roles
+     * @return
+     */
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        var now = new Date();
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now) // 토큰 생성시간
+                .setExpiration(expiredDate) //토큰 만료시간
+                .signWith(SignatureAlgorithm.HS512, this.secretKey) //사용할 암호화 알고리즘, 비밀키
+                .compact();
+    }
+    public String getUsername(String token){
+        return this.parseClaims(token).getSubject();
+    }
+    public boolean validateToken(String token){
+        if(!StringUtils.hasText(token)) return false;
+        return !this.parseClaims(token).getExpiration().before(new Date());
+    }
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(this.secretKey)
+                    .parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            // TODO
+            return e.getClaims();
+        }
+    }
 }
